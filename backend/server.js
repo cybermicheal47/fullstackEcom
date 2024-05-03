c89668608e6fedf819de03a3b0139b47d2dc9d15
cybermicheal47 <65826184+cybermicheal47@users.noreply.github.com>
2024-05-03 02:18:39 -0700

    .

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,10 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(
+  "frontend/public/uploads",
+  express.static(path.join(__dirname, "frontend/public//uploads"))
+);
 
 app.use(notFound);
 app.use(errorHandler);
